4b5796e7ba164c49bf4302b69db271c4e400baa2
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-06-19 12:27:59 +0200

    refactor: remove drizzle

--- src/db/schema/albums.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, uuid } from "drizzle-orm/pg-core";
-import { users } from "./users";
-
-export const albums = pgTable("albums", {
-  id: uuid("id").primaryKey(),
-  userID: serial("user_id").references(() => users.id),
-});
