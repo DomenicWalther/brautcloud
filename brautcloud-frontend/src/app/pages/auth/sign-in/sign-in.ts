d1661a75e5876e929563891c31e6f117b8eadbfb
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-03-02 21:16:45 +0100

    feat(frontend/events): Add a basic EventService that get's all Events

--- brautcloud-frontend/src/app/pages/auth/sign-in/sign-in.ts
+++ brautcloud-frontend/src/app/pages/auth/sign-in/sign-in.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth-service';
+import { EventService } from '../../../services/event-service';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,8 +11,13 @@ import { AuthService } from '../../../services/auth-service';
 })
 export class SignIn {
   private authService = inject(AuthService);
+  private eventService = inject(EventService);
 
   login() {
     this.authService.login();
+  }
+
+  getEvents() {
+    this.eventService.getEvents();
   }
 }
