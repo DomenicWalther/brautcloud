73685da68216de10a405ca747fdf8ec48be93ef1
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-03-08 00:15:00 +0100

    feat(FE::Image-Service): Add Page and EventImageDTO type to ImageService

--- brautcloud-frontend/src/app/services/image-service.ts
+++ brautcloud-frontend/src/app/services/image-service.ts
@@ -3,6 +3,22 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { API_URL } from '../core/tokens';
 
+export interface Page<T> {
+  content: T[];
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  number: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface EventImageDTO {
+  id: number;
+  url: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,9 +27,8 @@ export class ImageService {
   private http: HttpClient = inject(HttpClient);
   private readonly API_URL = inject(API_URL);
 
-
-  getEventImages(eventId: number, page: number, size = 20): Observable<any> {
-    return this.http.get<any>(`${this.API_URL}/events/${eventId}/images?page=${page}&size=${size}`, { withCredentials: true })
+  getEventImages(eventId: number, page: number, size = 20): Observable<Page<EventImageDTO>> {
+    return this.http.get<Page<EventImageDTO>>(`${this.API_URL}/events/${eventId}/images?page=${page}&size=${size}`, { withCredentials: true })
   }
 }
 
