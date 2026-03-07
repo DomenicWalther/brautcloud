1e478b2c85848b9c47c6707b03c7c48c4d2655f8
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-03-07 23:43:39 +0100

    fix(frontend): Remove unused constructor and fix issue where there might be no Event on an user

--- brautcloud-frontend/src/app/pages/app/home/home.ts
+++ brautcloud-frontend/src/app/pages/app/home/home.ts
@@ -1,29 +1,7 @@
-import { Component, computed, effect, inject, signal, Signal } from '@angular/core';
-import { HomeStats } from './home-stats/home-stats';
+import { Component, computed, inject } from '@angular/core';
 import { QrCodeComponent } from 'ng-qrcode';
 import { UserService } from '../../../services/user-service';
-import { toSignal } from '@angular/core/rxjs-interop';
-
-
-interface Event {
-  coupleName: string;
-  date: string;
-  eventName: string;
-  id: number;
-  location: string;
-  userID: number;
-}
-
-interface UserResponse {
-  createdAt: string;
-  email: string;
-  emailVerified: boolean;
-  events: Event[];
-  firstNameCoupleOne: string;
-  firstNameCoupleTwo: string;
-  id: number;
-  lastName: string;
-}
+import { HomeStats } from './home-stats/home-stats';
 
 @Component({
   selector: 'app-home',
@@ -37,19 +15,10 @@ export class Home {
 
   user = this.userService.user;
 
-  constructor() {
-    effect(() => {
-      const u = this.user();
-      if (u) {
-        console.log(u);
-      }
-    })
-  }
-
   daysTillWedding = computed(() => {
     const user = this.user();
     const date = new Date();
-    if (!user) return 0;
+    if (!user?.events?.length) return 0;
 
     const weddingDate = new Date(user.events[0].date);
     return Math.floor((weddingDate.getTime() - date.getTime()) / (1000 * 3600 * 24));
