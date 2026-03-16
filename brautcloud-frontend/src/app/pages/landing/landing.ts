ca8cf41ca20710bb00a99caf712f7b6cc6d22b46
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2026-03-16 12:54:02 +0100

    feat(FE::landing): Add fixed CTA to the Bottom of the Page

--- brautcloud-frontend/src/app/pages/landing/landing.ts
+++ brautcloud-frontend/src/app/pages/landing/landing.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import {LandingCtaSection} from './landing-cta-section/landing-cta-section';
 import {LandingExperienceSection} from './landing-experience-section/landing-experience-section';
 import {LandingHero} from './landing-hero/landing-hero';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-landing',
-  imports: [LandingCtaSection, LandingExperienceSection, LandingHero],
+  imports: [LandingCtaSection, LandingExperienceSection, LandingHero, RouterLink],
   templateUrl: './landing.html',
   standalone: true,
 })
