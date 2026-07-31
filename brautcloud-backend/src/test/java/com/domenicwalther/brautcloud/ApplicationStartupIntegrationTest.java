8c48319ffe05e74a1c4178675c9d2888c7152a30
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-07-31 22:41:01 +0200

    test: include existing guest session migration

--- brautcloud-backend/src/test/java/com/domenicwalther/brautcloud/ApplicationStartupIntegrationTest.java
+++ brautcloud-backend/src/test/java/com/domenicwalther/brautcloud/ApplicationStartupIntegrationTest.java
@@ -26,7 +26,7 @@ class ApplicationStartupIntegrationTest extends FullStackIntegrationTest {
 	void applicationStartsWithEveryMigrationAppliedToPostgres() {
 		assertThat(applicationContext.getBean(BrautcloudApplication.class)).isNotNull();
 		assertThat(Arrays.stream(flyway.info().applied()).map(info -> info.getVersion().getVersion()))
-			.containsExactly("1", "2", "3", "4", "5", "6", "7");
+			.containsExactly("1", "2", "3", "4", "5", "6", "7", "8");
 	}
 
 	@Test
